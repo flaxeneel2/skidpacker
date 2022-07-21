f727c01a36498ff032014966b8d46e669280afd0
slowrecall <slowrecall@protonmail.com>
2022-07-22 00:01:37 +0100

    Push changes

--- dev.skidpacker.loader/src/main/java/dev/skidpacker/loader/Main.java
+++ dev.skidpacker.loader/src/main/java/dev/skidpacker/loader/Main.java
@@ -1,12 +1,16 @@
 package dev.skidpacker.loader;
 
+import com.sun.jna.Native;
+
+import java.io.File;
+
 /*
 The java loader program that a user would be expected to run in order for the native
 module to work.
  */
 public class Main {
 
-    // The assumed init function in the native module.
+    // The init function in the native module.
     public static native void init();
 
     public static void main(String[] args) {
@@ -14,9 +18,12 @@ public class Main {
     }
 
     static {
-        /*
-        - The library will be called libskidpacker.
-         */
-        System.loadLibrary("resources/skidpacker");
+        try {
+            File file = Native.extractFromResourcePath("skidpacker");
+            System.load(file.getAbsolutePath());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
+
 }
