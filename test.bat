c99f9602afb97992bff9aa710a9ee0e17b17bf62
slowrecall <slowrecall@protonmail.com>
2022-07-23 17:55:33 +0100

    Change script to work on my device

--- test.bat
+++ test.bat
@@ -2,7 +2,7 @@
 echo Script cuz i was too lazy to auto build n update shit myself cuz lazy
 if %USERNAME%==siddh set mvn=C:\Users\siddh\apache-maven-3.8.6\bin\mvn.cmd
 Rem If your maven install is broken like mine and u wanna have to specify the path, uncommend the line below and set ur mvn
-Rem if %USERNAME%==uzair set mvn=C:\Users\uzair\mvn
+Rem if %USERNAME%==docto set mvn=C:\Users\docto\mvn
 cd dev.skidpacker.loader-jni/target/release
 cargo build --release
 cd ../../../dev.skidpacker.loader/src/main/resources/win32-x86-64
